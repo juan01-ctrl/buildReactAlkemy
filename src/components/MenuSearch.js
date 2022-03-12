ce9672d914c55364e36edfeb48b0681420f085e9
Juan Ierace <juanierace2@gmail.com>
2022-03-12 19:39:33 -0300

    first commit

--- src/components/MenuSearch.js
+++ src/components/MenuSearch.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const MenuSearch = ({setSearch}) => {
-    const [searchValue, setSearchValue] = useState('');
+const MenuSearch = ({ setSearch }) => {
+  const [searchValue, setSearchValue] = useState("");
 
-   
-
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        if(searchValue.length > 2){
-            setSearch(searchValue)
-        }
-      
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchValue.length > 2) {
+      setSearch(searchValue);
     }
+  };
   return (
-    <div className='menu__search'>
-        <form onSubmit={handleSubmit}>
-        <input type="text" name='search-recipes' className='menu__search-input'  placeholder="Search a recipe (by default, only some are shown)" onChange={(e)=>setSearchValue(e.target.value)}/>
-
-        </form>
+    <div className="menu__search">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search-recipes"
+          className="menu__search-input"
+          placeholder="Search a recipe (by default, only some are shown)"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MenuSearch
+export default MenuSearch;
